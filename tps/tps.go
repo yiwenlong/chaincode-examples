bd4bc036e7357502d684ef2a586242be54ad2129
yiwenlong <wlong.yi@gmail.com>
2020-06-30 15:32:08 +0800

    Modify tps chaincode: remove main function from tps.go. use log replace fmt as log printer.

--- tps/tps.go
+++ tps/tps.go
@@ -13,14 +13,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-package main
+package tps
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 	pb "github.com/hyperledger/fabric-protos-go/peer"
+	"log"
 )
 
 // Tps Chaincode implementation
@@ -28,13 +26,13 @@ type Tps struct {
 }
 
 func (t *Tps) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("Tps Init")
+	log.Println("Tps Init")
 	return shim.Success(nil)
 }
 
 func (t *Tps) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	fcn, params := stub.GetFunctionAndParameters()
-	fmt.Println("Invoke()", fcn, params)
+	log.Println("Invoke()", fcn, params)
 	if fcn == "put" {
 		stub.PutState(params[0], []byte(params[1]))
 		return shim.Success(nil)
@@ -46,21 +44,4 @@ func (t *Tps) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		return shim.Success(data)
 	}
 	return shim.Error("error")
-}
-
-func main() {
-	ccid := os.Args[1]
-	address := os.Args[2]
-	server := &shim.ChaincodeServer{
-		CCID:    ccid,
-		Address: address,
-		CC:      new(Tps),
-		TLSProps: shim.TLSProperties{
-			Disabled: true,
-		},
-	}
-	err := server.Start()
-	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
-	}
 }
